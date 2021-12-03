d53280862678526c26fee9513b556807758e7994
evaristorodrigues <evaristosrodrigues@gmil.com>
2021-12-03 01:23:00 -0300

    Removendo imports desnecessários

--- java8/src/br/com/evaristo/imperatvedeclarative/ImperativeVSDeclarative.java
+++ java8/src/br/com/evaristo/imperatvedeclarative/ImperativeVSDeclarative.java
@@ -3,7 +3,6 @@ package br.com.evaristo.imperatvedeclarative;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class ImperativeVSDeclarative {
